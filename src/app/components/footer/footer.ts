a074ff098ff81bac8bc7a035b8bce6a04e8581b5
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2025-10-29 10:47:39 +0100

    base footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface SocialLink {
+  icon: string;
+  url: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
-  standalone: true
+  styleUrls: ['./footer.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
 })
 export class Footer {
+  currentYear = new Date().getFullYear();
 
+  socialLinks = signal<SocialLink[]>([
+    {
+      icon: 'bi-linkedin',
+      url: 'https://it.linkedin.com/in/sefora-antonello-1587b6153',
+      label: 'LinkedIn',
+    },
+    {
+      icon: 'bi-whatsapp',
+      url: 'https://wa.me/393469730190',
+      label: 'Whatsapp',
+    },
+    {
+      icon: 'bi-envelope',
+      url: 'mailto:antonellosefora@gmail.com',
+      label: 'Gmail',
+    },
+  ]);
 }
