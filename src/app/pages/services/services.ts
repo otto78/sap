a360df77b6f6b33699df71c85bf3478fbdb84e26
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2025-10-30 18:01:48 +0100

    activity card component

--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Title } from "../../components/title/title";
+import { ActivityCard } from "../../components/activity-card/activity-card";
 
 @Component({
   selector: 'app-services',
-  imports: [Title],
+  imports: [Title, ActivityCard],
   templateUrl: './services.html',
   styleUrl: './services.scss',
   standalone: true
