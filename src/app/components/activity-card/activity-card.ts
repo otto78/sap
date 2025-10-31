b0aa0c16276130d10358ddadc6018dca4a0239b7
Andrea Mazzarotto <andrea.mazzarotto@gmail.com>
2025-10-31 11:04:17 +0100

    servizi schede e attività

--- src/app/components/activity-card/activity-card.ts
+++ src/app/components/activity-card/activity-card.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Activity } from '../../pages/services/services';
+import { Button } from '../button/button';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-activity-card',
-  imports: [],
+  standalone: true,
+  imports: [Button, CommonModule],
   templateUrl: './activity-card.html',
   styleUrl: './activity-card.scss',
 })
 export class ActivityCard {
-
+  activity = input.required<Activity>();
 }
